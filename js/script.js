d25bc621773b69a1f9bd7c2dfffefa9548412a59
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-11-16 12:27:27 -0700

    JS to build alerts and styles

--- js/script.js
+++ js/script.js
@@ -1,18 +1,45 @@
 // Gather elements
 const alertContainer = document.querySelector('.alert-container');
 
-const alertItem = document.createElement('div');
-// generateAlert(alertItem);
-
-alertContainer.appendChild(alertItem);
-
-function generateAlert(parentElement) {
+// Functions
+function generateAlert(parentElement, alertMsg) {
+	// Create 3 elements used in alert
+	const alertItem = document.createElement('div');
 	const span = document.createElement('span');
 	const p = document.createElement('p');
 
-	span.textContent = 'Alert';
-	p.innerText = 'This is a test';
+	// Add alert class to each alert div
+	alertItem.classList.add('alert');
 
-	parentElement.appendChild('span');
-	parentElement.appendChild('p');
+	// Add text and append to div
+	span.textContent = 'Alert';
+	alertItem.appendChild(span);
+
+	// Add text and append to div
+	p.innerText = alertMsg;
+	alertItem.appendChild(p);
+
+	// Create close button, add class, and append
+	const closeBtn = addButtonToElement(alertItem, 'x');
+	closeBtn.classList.add('alert-close');
+
+	// Append div to passed in parent element
+	parentElement.appendChild(alertItem);
+}
+
+function addButtonToElement(parentElement, buttonContent) {
+	const button = document.createElement('button');
+	button.textContent = buttonContent;
+	parentElement.appendChild(button);
+	return button;
+}
+
+// Build alerts
+const alertMessages = [
+	'Prow scuttle parrel provost Sail ho shrouds spirits boom mizzenmast yardarm.',
+	'Deadlights jack lad schooner scallywag dance the hempen jig carouser broadside cable strike colors.'
+];
+
+for (let message of alertMessages) {
+	generateAlert(alertContainer, message);
 }
